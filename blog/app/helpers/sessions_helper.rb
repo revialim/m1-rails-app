277aaceb332e3b4a33d59aa9d68f669751043a24
Lilian Hung <l.hung.m@gmail.com>
2016-11-08 16:37:29 +0100

    restricted access
    redirect to login page, unless logged in

--- blog/app/helpers/sessions_helper.rb
+++ blog/app/helpers/sessions_helper.rb
@@ -10,10 +10,16 @@ module SessionsHelper
 
 	def current_user
 		@current_user = @current_user || User.find_by(id: session[:user_id])
+		#session[:user_id].nil? ? nil : 
 	end
 
 	def logged_in?
 		!current_user.nil?
 	end
-
+	protected
+    def authorize
+      unless current_user
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 end
