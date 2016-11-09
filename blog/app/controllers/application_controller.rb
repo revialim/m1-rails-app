e69390fd9012272e9c26304ce62743d03ef89064
Lilian Hung <l.hung.m@gmail.com>
2016-11-09 10:22:14 +0100

    added delete to need login permission

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authorize, only: [:create, :edit, :delete, :new]
+  before_action :authorize, only: [:create, :edit, :destroy, :delete, :new]
   protect_from_forgery with: :exception
   include SessionsHelper
 end
